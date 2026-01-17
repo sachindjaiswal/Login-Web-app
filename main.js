f4e027ced8efad22cc889ea3c4ef20fb9fe1a15a
Harsh Salvi <harshsalvi250@gmail.com>
2026-01-17 20:55:51 +0530

    Added display of creds

--- main.js
+++ main.js
@@ -1,49 +1,54 @@
 import express from "express";
 import mongoose from "mongoose";
-import user from "./models/user.js"
+import user from "./models/user.js";
 import cors from "cors";
 
-
-const myApp = express()
+const myApp = express();
 myApp.use(cors());
-myApp.use(express.static("public"))
-myApp.use(express.json())
+myApp.use(express.static("public"));
+myApp.use(express.json());
 
 try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/myDatabase")
-    console.log("Database Connected");
+  const conn = await mongoose.connect("mongodb://localhost:27017/myDatabase");
+  console.log("Database Connected");
 } catch (error) {
-    console.log("error");
+  console.log("error");
 }
-myApp.get("/",(req,res)=>{
-    res.send("This is the initial End point of my Server ")
-})
+myApp.get("/", (req, res) => {
+  res.send("This is the initial End point of my Server ");
+});
 
-myApp.post("/login",async (req,res)=>{
-    let {username , password} = req.body
-    const existingUser =await  user.findOne({Name : username})
-    if(existingUser){
-        console.log('The user already exits')
-        return 
-    }else{
-
+myApp.post("/login", async (req, res) => {
+  let { username, password } = req.body;
+  const existingUser = await user.findOne({ Name: username });
+  if (existingUser) {
+    console.log("The user already exits");
+    return;
+  } else {
     const newUser = new user({
-        Name : username ,
-        password : password
-    })
-    await newUser.save()
-    console.log("User logined sucksexfully")
+      Name: username,
+      password: password,
+    });
+    await newUser.save();
+    console.log("User logined sucksexfully");
+  }
+});
 
-}})
+myApp.post("/retriveUser", async (req, res) => {
+  const { username } = req.body;
 
-myApp.post("/retriveUser",async (req,res)=>{
-    const {username} = req.body
-    const userDetail = await user.findOne({Name : username})
+  const userDetail = await user.findOne({ Name: username });
 
-    const userID = userDetail._id
-    console.log(userID);
-    
-})
-myApp.listen(3000,()=>{
-    console.log("The server has started");
-})
+  if (!userDetail) {
+    return res.json({ message: "User not found" });
+  }
+
+  res.json({
+    username: userDetail.Name,
+    password: userDetail.password,
+  });
+});
+
+myApp.listen(3000, () => {
+  console.log("The server has started");
+});
